1b1c1f25a72799de3132168dc00b08cad69eebd9
kirin <kirin.mzzt@gmail.com>
2025-08-09 03:42:21 +0500

    Подготовка посадочной страницы и 404-й

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NotFoundView from "@/views/NotFoundView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/*',
+      path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/:any(.*)',
+      name: 'notFound',
+      component: NotFoundView,
     },
   ],
 })
